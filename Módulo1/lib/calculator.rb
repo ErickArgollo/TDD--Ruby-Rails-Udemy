867ff31ce085ab0da2208894d62c151b4a52e6ad
Erick Argollo <argolloerickdev@gmail.com>
2022-12-06 11:37:48 -0300

    test: :sparkles: sum test

--- Módulo1/lib/calculator.rb
+++ Módulo1/lib/calculator.rb
@@ -1,4 +1,6 @@
 class Calculator 
+    # Esse sum é um método de instância, se fosse um método de classe, teria o self ( importante lembrar )
+    #Sendo um método de instância, eu posso substituir o meu context por um #sum <
     def sum(n1, n2)
         n1 + n2
     end 
--- Módulo1/spec/calculator/calculator_spec.rb
+++ Módulo1/spec/calculator/calculator_spec.rb
@@ -1,18 +1,19 @@
-require 'calculator'
+require "calculator"
 
-describe Calculator do 
-  it 'sum method for 2 numbers' do
-    calc = Calculator.new
-    result = calc.sum(5, 7)
-    expect(result).to eq(12)
+describe Calculator do
+  # É interessante utilizar o context caso eu tenha mais de um teste para a mesma classe, se tiver apenas um único teste, posso considerar utilizar o it direto. 
+  context "#sum" do # #sum pois é uma instância
+    it "positive numbers" do
+      calc = Calculator.new
+      result = calc.sum(5, 7)
+      expect(result).to eq(12)
+    end
+
+    # teste usando o not to
+    it "positive numbers with wrong answer" do
+      calc = Calculator.new
+      result = calc.sum(5, 7)
+      expect(result).not_to eq(11)
+    end
   end
-
-  # teste usando o o not to
-  it 'sum method for 2 numbers' do
-    calc = Calculator.new
-    result = calc.sum(5, 7)
-    expect(result).not_to eq(11)
-  end
-
-  
-end  
+end
