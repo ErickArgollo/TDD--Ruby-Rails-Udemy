6f9882f843787cdf31e2dca1c3f0949d7d3dde0d
Erick Argollo <argolloerickdev@gmail.com>
2022-12-06 11:44:44 -0300

    test: :memo: subject

--- Módulo1/spec/calculator/calculator_spec.rb
+++ Módulo1/spec/calculator/calculator_spec.rb
@@ -1,18 +1,19 @@
 require "calculator"
 
-describe Calculator do
+describe "#calculator" do
   # É interessante utilizar o context caso eu tenha mais de um teste para a mesma classe, se tiver apenas um único teste, posso considerar utilizar o it direto. 
+  subject(:calc) { Calculator.new() } # caso tivesse um initialize poderia passar aqui. 
+  #nessa primeira parte, estou de certa forma renomeando o subject, daí lá embaixo posso utilizar  result = calc.X
   context "#sum" do # #sum pois é uma instância
     it "positive numbers" do
-      calc = Calculator.new
-      result = calc.sum(5, 7)
+      result = subject.sum(5, 7)
+      # ao invés de ficar instanciando o objeto, posso simplesmente utilizar o subject. 
       expect(result).to eq(12)
     end
 
     # teste usando o not to
     it "positive numbers with wrong answer" do
-      calc = Calculator.new
-      result = calc.sum(5, 7)
+      result = calc.sum(5, 7) 
       expect(result).not_to eq(11)
     end
   end
